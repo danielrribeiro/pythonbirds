caf2810c1ea6ff003bfb3f32520c71d0532ad865
@danielrribeiro <danielrodriguesr@gmail.com>
2022-09-19 21:13:00 -0300

    Implementado o Carro

--- oo/carro.py
+++ oo/carro.py
@@ -67,7 +67,7 @@ O   L
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Norte'
-    >>> carro = Carro(direcao, motor)
+    >>> carro = Carro(motor, direcao)
     >>> carro.calcular_velocidade()
     0
      >>> carro.acelerar()
@@ -80,18 +80,39 @@ O   L
     >>> carro.calcular_velocidade()
     0
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_direita()
     >>> carro.calcular_direcao()
-    >>> 'Leste'
+    'Leste'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Oeste'
+    'Oeste'
 """
+class Carro:
+    def __init__(self, motor, direcao):
+        self.motor = motor
+        self.direcao = direcao
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 class Motor:
     def __init__(self):
